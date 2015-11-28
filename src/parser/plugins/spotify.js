223488380f47ab85e76cb5f56eecfac9dc31686f
Victor Gama <hey@vito.io>
2015-11-28 00:24:05 -0200

    Add plugins: Spotify and OpenGraph

---
+++ src/parser/plugins/spotify.js
@@ -0,0 +1,40 @@
+var Plugin = require('./baseplugin'),
+    request = require('request');
+
+class Spotify extends Plugin {
+
+    init() {
+        this.httpRegex = /(?:https?:\/\/)?open\.spotify\.com\/(album|track|user\/[^\/]+\/playlist)\/([a-zA-Z0-9]+)/;
+        this.uriRegex = /^spotify:(album|track|user:[^:]+:playlist):([a-zA-Z0-9]+)$/;
+    }
+
+    canHandle(url) {
+        return this.httpRegex.test(url) || this.uriRegex.test(url);
+    }
+
+    process(url, callback) {
+        this.logger.verbose('spotify', `processing ${url}`);
+        request.get({
+            url: `https://embed.spotify.com/oembed/?url=${url}`,
+            headers: {'User-Agent': 'request'}
+        }, (e, r, body) => {
+            if(e) {
+                this.logger.error('spotify', 'Error processing request: ', e);
+                callback(null);
+            } else {
+                try {
+                    callback({
+                        type: 'spotify',
+                        embed: JSON.parse(body).html
+                    });
+                } catch(ex) {
+                    this.logger.error('spotify', 'Error parsing response: ', ex);
+                    this.logger.error('spotify', `Body was: ${body}`);
+                    callback(null);
+                }
+            }
+        });
+    }
+}
+
+module.exports = Spotify;
