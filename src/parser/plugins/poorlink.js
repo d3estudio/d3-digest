b1ef232075173cfa161496a31d3f2a753a02766a
kaiomedau <k.medau@gmail.com>
2015-12-07 15:02:09 -0200

    Add scroll interation to base.html

--- src/parser/plugins/poorlink.js
+++ src/parser/plugins/poorlink.js
@@ -8,88 +8,64 @@ class PoorLink extends Plugin {
         return true;
     }
 
-    openGraph(body, url, callback) {
-        var result = {
-            title: body('meta[property="og:title"]').first().attr('content'),
-            summary: body('meta[property="og:description"]').first().attr('content'),
-            image: body('meta[property="og:image"]').first().attr('content')
+    init() {
+        this.processors = {
+            title: [
+                body => body('meta[property="og:title"]').first().attr('content'),                  // OGP
+                body => body('meta[name="twitter:title"]').attr('content'),                         // Twitter Card
+                body => body('meta[itemprop="name"]').first().attr('content') || body('title').first().text() || body('h1').first().text() || body('h2').first().text() || body('h3').first().text()
+            ],
+            summary: [
+                body => body('meta[property="og:description"]').first().attr('content'),            // OGP
+                body => body('meta[name="twitter:description"]').attr('content'),                   // Twitter Card
+                body => body('meta[itemprop="description"]').first().attr('content') || body('meta[name="description"]').first().attr('content')
+            ],
+            image: [
+                body => body('meta[property="og:image"]').first().attr('content'),                  // OGP
+                body => body('meta[name="twitter:image"]').attr('content'),                         // Twitter Card
+                body => body('meta[itemprop="image"]').first().attr('content') || body('div img').first().attr('src')
+            ]
         };
-
-        if(result.title && result.summary && result.image) {
-            callback(result);
-        } else {
-            callback(null);
-        }
-    }
-
-    twitterCard(body, url, callback) {
-        var result = {
-            title: body('meta[name="twitter:title"]').attr('content'),
-            summary: body('meta[name="twitter:description"]').attr('content'),
-            image: body('meta[name="twitter:image"]').attr('content')
-        };
-
-        if(result.title && result.summary && result.image) {
-            callback(result);
-        } else {
-            callback(null);
-        }
-    }
-
-    metaTags(body, url, callback) {
-        var result = {};
-        result.title = body('meta[itemprop="name"]').first().attr('content') ||
-                        body('title').first().text() ||
-                        body('h1').first().text() ||
-                        body('h2').first().text() ||
-                        body('h3').first().text();
-        result.summary = body('meta[itemprop="description"]').first().attr('content') ||
-                            body('meta[name="description"]').first().attr('content');
-        result.image = body('meta[itemprop="image"]').first().attr('content') ||
-                        body('div img').first().attr('src');
-
-        if(result.title && result.summary && result.image) {
-            callback(result);
-        } else {
-            callback(null);
-        }
     }
 
     process(url, callback) {
         this.logger.verbose('poorLink', `processing ${url}`);
         request.get(url, (e, r, body) => {
-            this.processors = [this.openGraph, this.twitterCard, this.metaTags];
-            if(e) {
+            if(e || r.statusCode !== 200) {
                 callback(null);
-                this.logger.error('poorLink', 'Error processing request: ', e);
+                this.logger.error('poorLink', `Error processing request: ${url}`, (e || 'Server error: ' + r.statusCode));
                 return;
             }
             var $ = cheerio.load(body);
-            var nextCallback = (r) => {
-                if(r) {
-                    r.type = 'rich-link';
-                    this.processImageSize(r, callback);
-                } else {
-                    callback(r);
-                }
-            };
-            this.execute($, url, nextCallback);
-        });
-    }
-
-    execute(body, url, callback, result) {
-        if(result) {
-            callback(result);
-            return;
-        } else {
-            var next = this.processors.shift();
-            if(next) {
-                next(body, url, (r) => this.execute(body, url, callback, r));
-            } else {
-                callback(null);
+            var result = {};
+            Object.keys(this.processors).forEach(k => {
+                this.processors[k].forEach(p => {
+                    if(!result[k]) {
+                        var r = p($);
+                        if(r) {
+                            result[k] = r;
+                        }
+                    }
+                });
+            });
+            if(result && result.title && result.image && result.summary) {
+                result.type = 'rich-link';
+                this.processImageSize(result, callback);
                 return;
+            } else {
+                var procs = this.processors.title,
+                    title = null;
+                procs.forEach(p => title |= p($));
+                result = {
+                    type: 'poor-link',
+                    title: title
+                };
+                if(!result.title) {
+                    result = null;
+                }
             }
-        }
+            callback(result);
+        });
     }
 
     processImageSize(result, callback) {
@@ -97,22 +73,22 @@ class PoorLink extends Plugin {
             url: result.image,
             encoding: null
         }, (error, response, body) => {
-            var orientation = 'vertical';
+            var orientation = 'vertical',
+                size = { width: undefined, height: undefined };
             if(!error && response.statusCode === 200) {
                 try {
                     var dim = sizeOf(body);
                     if(dim.width > dim.height) {
                         orientation = 'horizontal';
                     }
+                    size = dim;
                 } catch(err) {
                     this.logger.error('poorLink', 'Error processing image size: ', err);
                 }
-                result.imageOrientation = orientation;
-                callback(result);
-            } else {
-                result.imageOrientation = orientation;
-                callback(result);
             }
+            result.imageOrientation = orientation;
+            result.imageSize = size;
+            callback(result);
         });
     }
 }
