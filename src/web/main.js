cd7a691927e4e416f4fc909e6c41bf52cb8a9971
Victor Gama <hey@vito.io>
2015-12-17 03:16:08 -0200

    Update web entrypoint to receive remote notifications and prefetch new items

--- src/web/main.js
+++ src/web/main.js
@@ -3,13 +3,15 @@ var logger = require('npmlog'),
     Memcached = require('memcached'),
     Path = require('path'),
     fs = require('fs'),
-    moment = require('moment-timezone');
+    moment = require('moment-timezone'),
+    Redis = require('ioredis');
 
 var Settings = require('../shared/settings.js'),
     settings = new Settings(),
     Mongo = require('../shared/mongo'),
     PluginLoader = require('./plugins'),
-    Parser = require('./parser');
+    Parser = require('./parser'),
+    Processor = require('./processor');
 
 if(settings.loggerLevel) {
     logger.level = settings.loggerLevel;
@@ -30,9 +32,11 @@ var memcachedUrl = `${settings.memcachedHost}:${settings.memcachedPort}`,
     memcached = new Memcached(memcachedUrl),
     mongo = new Mongo(settings.mongoUrl, logger),
     app = express(),
-    parser = new Parser(logger, settings, plugins, mongo, memcached);
+    parser = new Parser(logger, settings, plugins, mongo, memcached),
+    redis = new Redis(settings.redisUrl);
 
 logger.info('web', `Using memcached @ ${memcachedUrl}`);
+logger.info('web', `Using redis @ ${settings.redisUrl}`);
 
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
@@ -47,11 +51,11 @@ app.get('/', (req, res) => {
     res.send(fs.readFileSync(index), 'text/html');
 });
 
-var handleRequest = function(res, rawPast, rawPresent) {
-    parser.itemsInRange(rawPresent, rawPast, (err, result) => {
+var handleRequest = function(res, skipping) {
+    parser.itemsInRange(skipping, (err, result) => {
         var response = {
-            from: rawPast,
-            until: rawPresent,
+            from: skipping,
+            next: skipping + settings.outputLimit + 1,
             items: result
         };
         if(err) {
@@ -64,27 +68,63 @@ var handleRequest = function(res, rawPast, rawPresent) {
 
 app.get('/api/latest', (req, res) => {
     logger.verbose('web', 'Request for api/latest');
-    var rawPresent = Date.now(),
-        past = moment(rawPresent).subtract(settings.outputDayRange, 'days'),
-        rawPast = past.toDate().valueOf();
-    handleRequest(res, rawPast, rawPresent);
+    handleRequest(res, 0);
 });
 
-app.get('/api/from/:from', (req, res) => {
-    logger.verbose('web', `Request for api/from/${req.params.from}`);
-    if(isNaN(req.params.from)) {
+app.get('/api/skip/:qtt', (req, res) => {
+    logger.verbose('web', `Request for api/skip/${req.params.qtt}`);
+    if(isNaN(req.params.qtt)) {
         res.status(403).send('Incorrect parameters.');
         return;
     }
-    var rawPresent = parseInt(req.params.from),
-        past = moment(rawPresent).subtract(settings.outputDayRange, 'days'),
-        rawPast = past.toDate().valueOf();
-    handleRequest(res, rawPast, rawPresent);
+    handleRequest(res, parseInt(req.params.qtt));
 });
 
-var server = app.listen(process.env.PORT || 2708, () => {
-  var host = server.address().address,
-      port = server.address().port;
+redis.subscribe('digest_notifications');
+redis.on('message', function (channel, message) {
+    try {
+        message = JSON.parse(message);
+    } catch(ex) {
+        logger.error('digestNotificationResponder', `Error: ${ex.message}`);
+        return;
+    }
 
-  logger.info(`WeeklyDigest Web Interface listening on http://${host}:${port}`);
+    if(message.type === 'emoji_changed') {
+        Processor.updateCustomEmojis(settings);
+    } else if(message.type === 'precache_item') {
+        mongo.perform((db, cb) => {
+            logger.verbose('digestNotificationResponder', `Trying to prefetch item with ts ${message.ts}`);
+            db.collection('items').findOne({ ts: message.ts }).then((doc) => {
+                if(doc) {
+                    logger.verbose('digestNotificationResponder', 'Handling to Processor');
+                    try {
+                        var docs = Parser.prepareDocuments(settings, [doc]);
+                        if(docs.length === 1) {
+                            var p = new Processor(settings, logger, plugins, memcached, docs);
+                            p.process(function() {
+                                logger.verbose('digestNotificationResponder', `Prefetch for ${message.ts} completed.`);
+                            });
+                        }
+                    } catch(ex) {
+                        logger.error('digestNotificationResponder', 'Message fake processing failed:');
+                        logger.error('digestNotificationResponder', ex);
+                    }
+                }
+            });
+            cb();
+        });
+    }
+});
+
+var server;
+
+mongo.perform((db, cb) => {
+    Processor.emojiCollection = db.collection('emoji');
+    cb();
+    server = app.listen(process.env.PORT || 2708, () => {
+        var host = server.address().address,
+        port = server.address().port;
+
+        logger.info(`WeeklyDigest Web Interface listening on http://${host}:${port}`);
+    });
 });
