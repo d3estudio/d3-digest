5f644475b76e6410673af429c3ade7fd3ecfd8aa
Victor Gama <hey@vito.io>
2015-12-20 02:40:17 -0200

    Add memcached shared class

---
+++ src/shared/memcached.js
@@ -0,0 +1,49 @@
+var _Memcached = require('memcached'),
+    settings = require('./settings').sharedInstance(),
+    Singleton = require('./singleton');
+
+class Memcached {
+    constructor() {
+        this.instance = new _Memcached(`${settings.memcachedHost}:${settings.memcachedPort}`);
+    }
+
+    get(key) {
+        return new Promise((resolve, reject) => {
+            this.instance.get(key, (err, d) => {
+                if(err) {
+                    reject(err);
+                } else {
+                    resolve(d);
+                }
+            });
+        });
+    }
+
+    set(key, value) {
+        return new Promise((resolve, reject) => {
+            this.instance.set(key, value, 2592000, (err) => {
+                if(err) {
+                    reject(err);
+                } else {
+                    resolve(value);
+                }
+            });
+        });
+    }
+
+    flush() {
+        return new Promise((resolve, reject) => {
+            this.instance.flush((err) => {
+               if(err) {
+                    reject(err);
+                } else {
+                    resolve();
+                }
+            });
+        })
+    }
+
+
+}
+
+module.exports = new Singleton(Memcached);
