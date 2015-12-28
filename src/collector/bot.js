b6a251d14e5c5d8e1ba5a7a1b1da19ea508d4352
Victor Gama <hey@vito.io>
2015-12-28 15:34:51 -0200

    Add documentation to Collector process sources

--- src/collector/bot.js
+++ src/collector/bot.js
@@ -3,7 +3,17 @@ var inflection = require('inflection'),
     Redis = require('ioredis'),
     logger = require('npmlog');
 
+/**
+ * Receives and preprocesses data coming from the Slack RTM
+ */
 class Bot {
+
+    /**
+     * Initialises a new instance of this class, preparing connections to Redis and filtering
+     * channels to be watched.
+     * @param  {Slack}  slack   Instance of the Slack node client
+     * @return {Bot} A new instance of this class
+     */
     constructor(slack) {
         this.slack = slack;
         this.redis = new Redis(settings.redisUrl);
@@ -50,12 +60,23 @@ class Bot {
             logger.info('Bot', `Okay, I will watch ${inflection.inflect('', validChannels.length, 'this', 'these')} ${inflection.inflect('channel', validChannels.length)}: ${validChannels.join(', ')}`);
         }
 
+        // Defines which message types must be accepted
         this.expectedMessages = ['message', 'reaction_added', 'reaction_removed', 'emoji_changed',
                                 'group_joined', 'message_deleted'];
+
+        /**
+         * Messages which type is defined in this array are checked against the list of watched
+         * channels by the `channelCheck` function.
+         */
         this.checkedCalls = ['reaction_added', 'reaction_removed', 'message_deleted', 'message_changed', 'message'];
         slack.on('raw_message', msg => this.guard(() => this.processMessage(msg)));
     }
 
+    /**
+     * Silly guard function that wraps function calls in a try/catch block
+     * @param  {Function}   func    Function to be wrapped in a try/catch block
+     * @return {undefined}
+     */
     guard(func) {
         try {
             func();
@@ -65,6 +86,11 @@ class Bot {
         }
     }
 
+    /**
+     * Checks whether a given message is coming from a watched channel
+     * @param  {object}     msg     Incoming slack message to be checked
+     * @return {bool}   Whether the message belongs to a watched channel or not.
+     */
     channelCheck(msg) {
         if(!msg.channel) {
             logger.warn('channelCheck', `Received empty or false-y channel ${msg.channel} for message with type ${msg.type}`, msg);
@@ -73,16 +99,38 @@ class Bot {
         return this.channels.indexOf(msg.channel) > -1;
     }
 
+    /**
+     * Processes an incoming slack message
+     * @param  {object}     msg     Message to be processed
+     * @return {undefined}
+     */
     processMessage(msg) {
+
+        /*
+         * Normalise message types. For instance, a `message_deleted` type is a subtype of
+         * `message`, but the subtype information is enough for us.
+         */
         if(msg.subtype) {
             msg.type = msg.subtype;
         }
+        // Discard any message that is not of the expected type
         if(this.expectedMessages.indexOf(msg.type) === -1) {
             return;
         }
+
+
         if(msg.type === 'emoji_changed') {
+            /*
+             * When the custom emoji list is updated, we publish a notification on the predefined
+             * channel, that is also listened by other processes (in this case, the `prefetch`
+             * process). This will force the process to reload the custom emoji list from Slack.
+             */
             this.redis.publish(settings.notificationChannel, 'emoji_changed');
         } else if(msg.type === 'group_joined') {
+            /**
+             * If the bot is invited to a new group or channel, and is configured to auto-watch,
+             * watch it and save settings.
+             */
             if(!settings.autoWatch) {
                 return;
             }
@@ -97,7 +145,10 @@ class Bot {
             var serializable = {},
                 fields = ['channel', 'team', 'text', 'ts', 'type', 'user', 'event_ts', 'item',
                         'reaction', 'message', 'deleted_ts'];
-
+            /*
+             * In order to serialise the message coming from Slack, we must trim some properties.
+             * Here we keep only the essential information that may be useful to other processes.
+             */
             fields.forEach((k) => {
                 if(msg.hasOwnProperty(k)) {
                     serializable[k] = msg[k];
@@ -131,6 +182,7 @@ class Bot {
             if(Object.keys(serializable).length > 0 && (this.checkedCalls.indexOf(serializable.type) > -1 && this.channelCheck(serializable))) {
                 var serialized = JSON.stringify(serializable);
                 logger.verbose('collector', `rpushing to ${settings.processQueueName}: ${serialized}`);
+                // Push item to the processor queue.
                 this.redis.rpush(settings.processQueueName, serialized);
             }
         }
